aa79f0e7f3ead1804cbeeaf8cd09b9d5d98e7a48
優初 <yuu623popfancy@gmail.com>
2020-09-23 12:25:45 +0900

    skyway

--- main.js
+++ main.js
@@ -184,7 +184,7 @@ $(() => {
              $("#local-video-canvas").html('<img src="img/smile.png">');
            }else if(max==numbers[5]){
              console.log("neutral");
-             $("#local-video-canvas").html('<img src="img/smile.png">');
+             $(".stamp").html('<img src="img/smile.png">');
            }else if(max==numbers[6]){
              console.log("sadness");
              $("#local-video-canvas").html('<img src="img/smile.png">');
